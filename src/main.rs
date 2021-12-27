8a2da759377c58d61c55ef4aa967a43b3aef89ce
yatoogamii <bracaval.elias.pro@gmail.com>
2021-12-27 08:45:50 +0100

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let mut pattern: String = String::new();
+
+    for (index, arg) in std::env::args().enumerate() {
+        if index == 1 {
+            pattern = arg;
+        } else if index > 1 {
+            match std::fs::read_to_string(&arg) {
+                Ok(file) => {
+                    println!("Matches in the file: {}", &arg);
+
+                    for (index, line) in file.lines().enumerate() {
+                        if line.contains(&pattern) {
+                            let col = line.find(&pattern).unwrap();
+                            let content_match =
+                                &line[col..line[col..].find(" ").unwrap_or(line.len()) + col];
+
+                            println!("row {} / col {} \n {:#?}", index, col, content_match);
+                        }
+                    }
+                }
+                Err(_) => println!("Can't read file at the path: {}", arg),
+            };
+        }
+    }
 }
